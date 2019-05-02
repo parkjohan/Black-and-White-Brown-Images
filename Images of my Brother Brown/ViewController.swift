9b6afa2fe47b3e727e12b04e382ae2001d979777
Johan Park <parkjohan@gmail.com>
2019-05-01 17:44:32 -0700

    added images added Detail View Controller to display selectedImage

--- Images of my Brother Brown/ViewController.swift
+++ Images of my Brother Brown/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Brown in Black & White"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.sizeToFit()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("JPG") {
+                pictures.append(item)
+                pictures.sort()
+            }
+            print(pictures)
+        }
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = pictures[index]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //try loading the destination view controller with identifier and typecasting it as DetailViewController
+        if let destVC = storyboard?.instantiateViewController(withIdentifier: "Detail View Controller") as? DetailViewController {
+            
+            //if successful, load the appropriate data into the destVC properties
+            destVC.selectedImage = pictures[indexPath.row]
+            destVC.selectedPictureNumber = indexPath.row + 1
+            destVC.totalPictureCount = pictures.count
+            
+            //push the destVC onto the navigation controller
+            navigationController?.pushViewController(destVC, animated: true)
+        }
+    }
 
 }
 
